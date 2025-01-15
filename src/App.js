154b4b9eb4eac58ef7c72dbedc5148791125c365
Aman <amantandon21december@gmail.com>
2025-01-15 16:15:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl text-green-800 font-bold">
+      Namaste Everyone, let build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
